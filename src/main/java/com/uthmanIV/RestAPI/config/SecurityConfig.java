713241e06bf3009725c34618bb97413f977e68fe
Uthman Yahaya Baba <abustuff@gmail.com>
2024-09-08 22:01:47 +0100

    Implemented security using custom defined table

--- src/main/java/com/uthmanIV/RestAPI/config/SecurityConfig.java
+++ src/main/java/com/uthmanIV/RestAPI/config/SecurityConfig.java
@@ -6,9 +6,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,7 +17,12 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource){
-        return new JdbcUserDetailsManager(dataSource);
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select username, password, enabled from users where username=?");
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select username, authority from authorities where username =?");
+        return jdbcUserDetailsManager;
     }
 
     @Bean
